fc4cbe62c3f381ee3fa49c027e2c784974bcb99f
jiro4989 <jiroron666@gmail.com>
2026-06-11 22:37:10 +0900

    add: unit test code

---
+++ test/eltimer-test.el
@@ -0,0 +1,63 @@
+;;; eltimer-test.el --- Tests for eltimer.el -*- lexical-binding: t; -*-
+;;
+;;; Commentary:
+;; Unit test for eltimer.el.
+;;
+;;; Code:
+
+(require 'ert)
+(require 'eltimer)
+
+(ert-deftest eltimer-test-eltimer-number-to-time-format-1 ()
+  (should (string= "00:00:01" (eltimer-number-to-time-format 1))))
+
+(ert-deftest eltimer-test-eltimer-number-to-time-format-59 ()
+  (should (string= "00:00:59" (eltimer-number-to-time-format 59))))
+
+(ert-deftest eltimer-test-eltimer-number-to-time-format-60 ()
+  (should (string= "00:01:00" (eltimer-number-to-time-format 60))))
+
+(ert-deftest eltimer-test-eltimer-number-to-time-format-61 ()
+  (should (string= "00:01:01" (eltimer-number-to-time-format 61))))
+
+(ert-deftest eltimer-test-eltimer-number-to-time-format-3599 ()
+  (should (string= "00:59:59" (eltimer-number-to-time-format 3599))))
+
+(ert-deftest eltimer-test-eltimer-number-to-time-format-3600 ()
+  (should (string= "01:00:00" (eltimer-number-to-time-format 3600))))
+
+(ert-deftest eltimer-test-eltimer-number-to-time-format-3601 ()
+  (should (string= "01:00:01" (eltimer-number-to-time-format 3601))))
+
+(ert-deftest eltimer-test-eltimer-number-to-time-format-3661 ()
+  (should (string= "01:01:01" (eltimer-number-to-time-format 3661))))
+
+(ert-deftest eltimer-test-eltimer-parse-string-123 ()
+  (should (equal '(:hour 1 :minute 2 :second 3) (eltimer-parse-string "1h 2m 3s"))))
+
+(ert-deftest eltimer-test-eltimer-parse-string-hours ()
+  (should (equal '(:hour 1 :minute 0 :second 0) (eltimer-parse-string "1 hours"))))
+
+(ert-deftest eltimer-test-eltimer-parse-string-minutes ()
+  (should (equal '(:hour 0 :minute 2 :second 0) (eltimer-parse-string "2 minutes"))))
+
+(ert-deftest eltimer-test-eltimer-parse-string-seconds ()
+  (should (equal '(:hour 0 :minute 0 :second 33) (eltimer-parse-string "33 seconds"))))
+
+(ert-deftest eltimer-test-eltimer-parse-string-0 ()
+  (should (equal '(:hour 0 :minute 0 :second 0) (eltimer-parse-string ""))))
+
+(ert-deftest eltimer-test-eltimer-time-to-seconds-123 ()
+  (should (= 3723 (eltimer-time-to-seconds '(:hour 1 :minute 2 :second 3)))))
+
+(ert-deftest eltimer-test-eltimer-time-to-seconds-hour ()
+  (should (= 3600 (eltimer-time-to-seconds '(:hour 1 :minute 0 :second 0)))))
+
+(ert-deftest eltimer-test-eltimer-time-to-seconds-minute ()
+  (should (= 120 (eltimer-time-to-seconds '(:hour 0 :minute 2 :second 0)))))
+
+(ert-deftest eltimer-test-eltimer-time-to-seconds-second ()
+  (should (= 3 (eltimer-time-to-seconds '(:hour 0 :minute 0 :second 3)))))
+
+(provide 'eltimer-test)
+;;; eltimer-test.el ends here
